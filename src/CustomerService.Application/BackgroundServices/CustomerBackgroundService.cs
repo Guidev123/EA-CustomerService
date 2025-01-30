fa1ed6c2fa14e0d4ca46525007f213268b289599
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-01-30 19:13:41 -0300

    feat: recebendo integration event para endereco

--- src/CustomerService.Application/BackgroundServices/CustomerBackgroundService.cs
+++ src/CustomerService.Application/BackgroundServices/CustomerBackgroundService.cs
@@ -1,7 +1,9 @@
-﻿using CustomerService.Application.Commands.Create;
+﻿using CustomerService.Application.Commands.AddAddress;
+using CustomerService.Application.Commands.Create;
 using CustomerService.Application.Commands.Delete;
 using EA.IntegrationEvents.Integration;
 using EA.IntegrationEvents.Integration.DeletedUser;
+using EA.IntegrationEvents.Integration.ReceivedAddress;
 using EA.IntegrationEvents.Integration.RegisteredUser;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +23,7 @@ namespace CustomerService.Application.BackgroundServices
         {
             _bus.RespondAsync<RegisteredUserIntegrationEvent, ResponseMessage>(RegisterCustomer);
             _bus.RespondAsync<DeletedUserIntegrationEvent, ResponseMessage>(DeleteCustomer);
+            _bus.SubscribeAsync<ReceivedAddressIntegrationEvent>("ReceivedAddressIntegrationEvent", AddAddress);
             _bus.AdvancedBus.Connected += OnConnect!;
         }
 
@@ -50,6 +53,22 @@ namespace CustomerService.Application.BackgroundServices
         {
             var clientCommand = new DeleteCustomerCommand(message.Id);
             Response<DeleteCustomerCommand> success;
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var mediator = scope.ServiceProvider.GetRequiredService<IMediatorHandler>();
+
+                success = await mediator.SendCommand(clientCommand);
+            }
+
+            return new ResponseMessage(success.Data!.ValidationResult!);
+        }
+
+        private async Task<ResponseMessage> AddAddress(ReceivedAddressIntegrationEvent message)
+        {
+            var clientCommand = new AddAddressCommand(message.Street, message.Number, message.AdditionalInfo,
+                                message.Neighborhood, message.ZipCode, message.State, message.City);
+
+            Response<AddAddressCommand> success;
             using (var scope = _serviceProvider.CreateScope())
             {
                 var mediator = scope.ServiceProvider.GetRequiredService<IMediatorHandler>();
